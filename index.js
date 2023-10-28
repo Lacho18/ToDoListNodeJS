b679049c814ca67bb162c7ec41ce99ec2433b57c
LachezarGenov <121165891+Lacho18@users.noreply.github.com>
2023-10-28 21:41:59 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const PORT = 5000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '/public')));
+app.use('/', require('./routes/root'));
+
+app.listen(PORT, () => {console.log('App is running on port ' + PORT)});
