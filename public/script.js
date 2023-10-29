2de18b726e353d4c4ac5b6f90592b16e5f29e65a
LachezarGenov <121165891+Lacho18@users.noreply.github.com>
2023-10-29 12:55:31 +0200

    second commit

---
+++ public/script.js
@@ -0,0 +1,66 @@
+let tasksList = document.getElementById('tasks');
+let addMenu = document.getElementById('addMenu');
+let AddButton = document.getElementById('AddButton');
+let addingForm = document.getElementById('addingForm');
+
+let menuButton = document.getElementById('menuButton');
+let menuDiv = document.getElementById('menuDiv');
+let menuShown = false;
+menuDiv.style.display = 'none';
+
+let bodyObject = {
+    task : "",
+    importance : 0
+};
+
+addMenu.style.display = "none";
+
+AddButton.addEventListener('click', () => {
+    let allElements = addMenu.querySelectorAll("*");
+    allElements.forEach(indexValue => {
+        indexValue.style.display = 'none';
+    });
+    addMenu.style.display = "inline-block";
+    addMenu.classList.add('addMenuApearence');
+    setTimeout(() => {
+        allElements.forEach(indexValue => {
+            indexValue.style.display = 'inline-block';
+        });
+        addMenu.classList.remove('addMenuApearence');
+    }, 2000);
+});
+
+menuButton.addEventListener('click', () => {
+    let everyButton = menuDiv.querySelectorAll("*");
+    everyButton.forEach(indexValue => {indexValue.style.display = "none";});
+    if(!menuShown) {
+        menuDiv.style.display = 'inline-block';
+        menuDiv.classList.add('menuApearence');
+        menuShown = true;
+        setTimeout(() => {
+            everyButton.forEach(indexValue => {indexValue.style.display = "inline-block";});
+        }, 2000);
+    }
+    else {
+        menuDiv.style.display = 'none';
+        menuDiv.classList.remove('menuApearence');
+        menuShown = false;
+    }
+})
+
+addingForm.addEventListener('submit', () => {
+    let taskContect = document.getElementById('newTask');
+    let importanceContect = document.getElementById('importance');
+
+    bodyObject.task = taskContect.value;
+    bodyObject.importance = parseInt(importanceContect.value);
+
+    fetch('/submit', {
+        method: 'POST',
+        body: bodyObject,
+    })
+    .then((response) => {response.text()})
+    .then((data) => {console.log(data)});
+
+    bodyObject = {};
+})
--- routes/root.js
+++ routes/root.js
@@ -6,4 +6,10 @@ router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'doc.html'));
 });
 
+router.post('/submit', (req, res) => {
+    const body = req.body;
+    console.log('Received Data:', body);
+    res.send('Data received by the server');
+})
+
 module.exports = router;
